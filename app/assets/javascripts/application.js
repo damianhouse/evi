66e5b87e506960e32441d2e4b742640bdc0e5bdc
damianhouse <damianhouse@gmail.com>
2016-08-26 23:25:54 -0400

    Adjust application js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
