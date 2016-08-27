2a70e5ca3bc53e08b2a25447e1bcc52ab7b95151
damianhouse <damianhouse@gmail.com>
2016-08-26 23:51:46 -0400

    Refactor end_time validation for appointments to be only less than. Use collection_selects for patient id and interpreter

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :patient
-  validates_presence_of :start_time, :end_time
+  validates_presence_of :start_time, :end_time, :clinic, :patient_id
   validate :end_time_is_after_start_time
 
   private
@@ -9,7 +9,7 @@ class Appointment < ApplicationRecord
   def end_time_is_after_start_time
     return if end_time.blank? || start_time.blank?
 
-    if end_time <= start_time
+    if end_time < start_time
       errors.add(:end_time, "Cannot be before the start time")
     end
   end
