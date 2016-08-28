82c18cdfc07e4653d6151c46b54aa89be3700820
damianhouse <damianhouse@gmail.com>
2016-08-28 00:15:55 -0400

    Refactor user header current user logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def is_admin?
-    current_user.admin
-  end
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:first_name, :last_name, :email, :password)
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:first_name, :last_name, :email, :password)
+      end
     end
-  end
 end
