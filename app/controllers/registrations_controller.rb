697955e78ef05b717108614f34fa08b731d4a8c2
damianhouse <damianhouse@gmail.com>
2016-09-08 15:50:44 -0400

    Adjust mobile calendar styling. Add list appointments to the calendar page.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
     respond_to :json, :js
     prepend_before_filter :require_no_authentication, only: [:cancel ]
 
+    protected
+
+    def sign_up(resource_name, resource)
+      sign_in(resource_name, resource) unless current_user
+    end
 end
