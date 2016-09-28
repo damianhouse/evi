8dccd7751182272cb6997b46adb649ee55840103
damianhouse <damianhouse@gmail.com>
2016-09-28 13:11:54 -0400

    Add default 0.0 to hourly_rate for users.

--- db/migrate/20160922163123_add_hourly_rate_to_user.rb
+++ db/migrate/20160922163123_add_hourly_rate_to_user.rb
@@ -1,5 +1,5 @@
 class AddHourlyRateToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :hourly_rate, :float
+    add_column :users, :hourly_rate, :float, default: 0.0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 20160922163123) do
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "admin",                  default: false
-    t.float    "hourly_rate"
+    t.float    "hourly_rate",            default: 0.0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
