8296f7a6fa57cf587d6d13197a8f805df65ba83d
damianhouse <damianhouse@gmail.com>
2016-10-23 15:36:06 -0400

    Adjust number of variables in SQL statement for auto create.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -27,11 +27,11 @@ class InvoicesController < ApplicationController
 
   def auto_create
     @appointments = Appointment.where('complete = ? AND invoice_id IS NULL', true).where.not(user_id: nil)
-    @users = @appointments.to_a.map { |x| User.find(x.user_id)}
+    @users = @appointments.map { |x| User.find(x.user_id)}
     @users = @users.uniq
     @invoices = []
     @users.each do |user|
-      appointments = Appointment.all.where('complete = ? AND invoice_id IS NULL AND user_id = ?', true, nil, user.id)
+      appointments = Appointment.all.where('complete = ? AND AND user_id = ? invoice_id IS NULL' ,true, user.id)
       invoice = Invoice.new(user_id: user.id, start_date: appointments.order('start_time asc').limit(1)[0].start_time.strftime("%y-%m-%d"), end_date: appointments.order('start_time desc').limit(1)[0].start_time.strftime("%y-%m-%d"))
       invoice.miles_total = appointments.reduce(0){|sum, x| sum + x.miles_driven}
       invoice.hours_total = appointments.reduce(0){|sum, x| sum + (x.end_time - x.start_time) / (60*60)}
