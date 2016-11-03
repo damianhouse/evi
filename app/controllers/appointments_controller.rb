3d2772ce4fe0da1f4f15e6eb8a744601713dcff2
damianhouse <damianhouse@gmail.com>
2016-11-03 09:34:17 -0400

    DRY up appointments controller code for updating invoice when changing/deleting appointments.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -58,15 +58,7 @@ class AppointmentsController < ApplicationController
   # PATCH/PUT /appointments/1.json
   def update
     @appointment.update(appointment_params)
-    if @appointment.invoice_id.present?
-      user = User.find(@appointment.user_id)
-      @invoice = Invoice.find(@appointment.invoice_id)
-      appointments = Appointment.where('invoice_id = ?', @appointment.invoice_id)
-      @invoice.miles_total = appointments.reduce(0){|sum, x| sum + x.miles_driven}
-      @invoice.hours_total = appointments.reduce(0){|sum, x| sum + (x.end_time - x.start_time) / (60*60)}
-      @invoice.total_paid = ((@invoice.hours_total * user.hourly_rate) + (@invoice.miles_total * 1))
-      @invoice.save!
-    end
+    update_invoice(@appointment) if @appointment.invoice_id.present?
     @appointment.save!
     @appointemnts = Appointment.all
   end
@@ -75,6 +67,7 @@ class AppointmentsController < ApplicationController
   # DELETE /appointments/1.json
   def destroy
     @appointment.destroy
+    update_invoice(@appointment) if @appointment.invoice_id.present?
 
     @appointments = Appointment.all
     redirect_to root_path
@@ -86,7 +79,15 @@ class AppointmentsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-
+    def update_invoice(appointment)
+      user = User.find(@appointment.user_id)
+      @invoice = Invoice.find(@appointment.invoice_id)
+      appointments = Appointment.where('invoice_id = ?', @appointment.invoice_id)
+      @invoice.miles_total = appointments.reduce(0){|sum, x| sum + x.miles_driven}
+      @invoice.hours_total = appointments.reduce(0){|sum, x| sum + (x.end_time - x.start_time) / (60*60)}
+      @invoice.total_paid = ((@invoice.hours_total * user.hourly_rate) + (@invoice.miles_total * 1))
+      @invoice.save!
+    end
     def set_appointments
       @appointments = Appointment.all
     end
